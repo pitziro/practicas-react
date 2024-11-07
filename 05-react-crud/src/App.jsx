2a56b7fdc43ade01505c6c64a6b626a3900a4e28
pitziro <msarmientor1@gmail.com>
2024-11-07 00:45:45 -0500

    fixed:tailwind+tremor

--- 05-react-crud/src/App.jsx
+++ 05-react-crud/src/App.jsx
@@ -1,7 +1,13 @@
 import './App.css'
+import UsersTable from './components/LisfofUsers'
 
 function App() {
-	return <> react crud</>
+	return (
+		<>
+			<h1 className='text-3xl font-bold text-green-500 underline'>Hello tremor!</h1>
+			<UsersTable />
+		</>
+	)
 }
 
 export default App
